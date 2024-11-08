a5a085d3d2036adfc4e69f39d987acd023e4ed53
Jayce Olson <jolson20@wgu.edu>
2024-11-08 09:53:06 -0800

    Initial Commit Task A & B

---
+++ src/test/java/com/example/demo/D288ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D288ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
